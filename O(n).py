9f49ffc78b89ee53cc32da85e44bf01d9cdc4905
Emiliana Geronimo <100717136+Emmyspecialg@users.noreply.github.com>
2023-07-10 09:32:14 -0400

    Add files via upload

---
+++ O(n).py
@@ -0,0 +1,19 @@
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
+# there are 2 n's and 2 i's. the function passes over it 10 times. 
+# n is the number of operations 
+#
+           
+              
+                  
+                  
+
+
+
+
+
+
